58894ac0e3be2a481fcaf90d1178be135d201207
nscando <nicolas.scandizzo@gmail.com>
2021-02-27 10:05:14 -0300

    Script db modif

---
+++ multiverse-db/examples/index.js
@@ -0,0 +1,61 @@
+'use strict'
+const chalk = require('chalk')
+const db = require('../')
+
+async function run() {
+  const config = {
+    database: process.env.DB_NAME || 'multiverse',
+    username: process.env.DB_USER || 'nico',
+    password: process.env.DB_PASS || 'nico',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+  }
+
+  const { Agent, Metric } = await db(config).catch(handleFatalError)
+
+  const agent = await Agent.createOrUpdate({
+    uuid: 'jjj',
+    name: 'nico',
+    username: 'nico',
+    hostname: 'nico',
+    pid: 3,
+    connected: true,
+  }).catch(handleFatalError)
+
+  console.log('--agent--')
+  console.log(agent)
+
+  const agents = await Agent.findAll().catch(handleFatalError)
+  console.log('--agents--')
+  console.log(agents)
+
+  const metrics = await Metric.findByAgentUuid(agent.uuid).catch(
+    handleFatalError
+  )
+  console.log('--metrics--')
+  console.log(metrics)
+
+  const metric = await Metric.create(agent.uuid, {
+    type: 'memory',
+    value: '300',
+  }).catch(handleFatalError)
+
+  console.log('--metrics--')
+  console.log(metric)
+
+  const metricsType = await Metric.findByTypeAgentUuid(
+    'memory',
+    agent.uuid
+  ).catch(handleFatalError)
+
+  console.log('--metrics--')
+  console.log(metricsType)
+}
+
+function handleFatalError(err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+run()
--- multiverse-db/setup.js
+++ multiverse-db/setup.js
@@ -7,17 +7,29 @@ const db = require('./index')
 
 const prompt = inquirer.createPromptModule()
 
-async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your DB, are you sure?'
-    }
-  ])
+async function setup() {
+  let flag = false
 
-  if (!answer.setup) {
-    return console.log('Nothing happened!')
+  process.argv.forEach((e) => {
+    if (e === '--yes') {
+      flag = true
+    }
+    if (e === '--y') {
+      flag = true
+    }
+  })
+
+  if (!flag) {
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your DB, are you sure?',
+      },
+    ])
+    if (!answer.setup) {
+      return console.log('Nothing happened!')
+    }
   }
 
   const config = {
@@ -27,7 +39,7 @@ async function setup () {
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: (s) => debug(s),
-    setup: true
+    setup: true,
   }
   await db(config).catch(handleFatalError)
 
@@ -36,7 +48,7 @@ async function setup () {
   process.exit(0)
 }
 
-function handleFatalError (err) {
+function handleFatalError(err) {
   console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)

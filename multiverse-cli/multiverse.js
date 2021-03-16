0e67a36661353675a86ffc5571296cbec67a3818
nscando <nicolas.scandizzo@gmail.com>
2021-03-16 11:56:26 -0300

    Monitoring agent integration

--- multiverse-cli/multiverse.js
+++ multiverse-cli/multiverse.js
@@ -14,6 +14,11 @@ const screen = blessed.screen()
 
 const agents = new Map()
 const agentMetrics = new Map()
+let extended = []
+let selected = {
+  uuid: null,
+  type: null,
+}
 
 const grid = new contrib.grid({
   rows: 1,
@@ -32,12 +37,9 @@ const line = grid.set(0, 1, 1, 3, contrib.line, {
   xPadding: 5,
 })
 
-screen.key(['escape', 'q', 'C-c'], (ch, key) => {
-  process.exit(0)
-})
-
 agent.on('agent/connected', (payload) => {
   const { uuid } = payload.agent
+
   if (!agents.has(uuid)) {
     agents.set(uuid, payload.agent)
     agentMetrics.set(uuid, {})
@@ -63,10 +65,11 @@ agent.on('agent/message', (payload) => {
 
   if (!agents.has(uuid)) {
     agents.set(uuid, payload.agent)
-    agentMetrics(uuid, {})
+    agentMetrics.set(uuid, {})
   }
 
   const metrics = agentMetrics.get(uuid)
+
   payload.metrics.forEach((m) => {
     const { type, value } = m
 
@@ -75,25 +78,46 @@ agent.on('agent/message', (payload) => {
     }
 
     const length = metrics[type].length
-
     if (length >= 20) {
       metrics[type].shift()
     }
+
     metrics[type].push({
       value,
       timestamp: moment(timestamp).format('HH:mm:ss'),
     })
   })
+
   renderData()
+})
+
+tree.on('select', (node) => {
+  const { uuid } = node
+
+  if (node.agent) {
+    node.extended
+      ? extended.push(uuid)
+      : (extended = extended.filter((e) => e !== uuid))
+    selected.uuid = null
+    selected.type = null
+    return
+  }
+  selected.uuid = uuid
+  selected.type = node.type
+
+  renderMetric()
 })
 
 function renderData() {
   const treeData = {}
+  let idx = 0
+
   for (let [uuid, val] of agents) {
-    const title = `${val.name} - (${val.pid})`
+    const title = ` ${val.name} - (${val.pid})`
     treeData[title] = {
       uuid,
       agent: true,
+      extended: extended.includes(uuid),
       children: {},
     }
 
@@ -105,8 +129,8 @@ function renderData() {
         metric: true,
       }
 
-      const metricName = `${type}`
-      treedata[title].children[metricName] = metric
+      const metricName = ` ${type} ${'  '.repeat(1000)} ${idx++}`
+      treeData[title].children[metricName] = metric
     })
   }
 
@@ -114,8 +138,34 @@ function renderData() {
     extended: true,
     children: treeData,
   })
+  renderMetric()
+}
+
+function renderMetric() {
+  if (!selected.uuid && !selected.type) {
+    line.setData([{ x: [], y: [], title: '' }])
+    screen.render()
+    return
+  }
+
+  const metrics = agentMetrics.get(selected.uuid)
+  const values = metrics[selected.type]
+
+  const series = [
+    {
+      title: selected.type,
+      x: values.map((v) => v.timestamp).slice(-10),
+      y: values.map((v) => v.value).slice(-10),
+    },
+  ]
+  line.setData(series)
   screen.render()
 }
 
+screen.key(['escape', 'q', 'C-c'], (ch, key) => {
+  process.exit(0)
+})
+
 agent.connect()
+tree.focus()
 screen.render()
